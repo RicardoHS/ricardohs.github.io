07c194c73789e0283b84da44e21c47d2b778d6d5
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2021-01-20 10:28:19 +0100

    add loading placeholder on menu click

--- js/main.js
+++ js/main.js
@@ -1,10 +1,11 @@
-console.log('js is working')
+console.log('js is working');
 
 $(document).ready(function () {
 
     $('.menu-item').click(function () {
-        dataFile = $(this).attr('data-file')
-        dataFile = `content/${dataFile}`
+        dataFile = $(this).attr('data-file');
+        dataFile = `content/${dataFile}`;
+        $('section.content').html('Loading...');
         $.get(dataFile, function (data) {
             $('section.content').html(marked(data));
         }, "text");
