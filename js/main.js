2a13a2d20e2c393efaa4ac85b64ee0ff99236e06
Ricardo Hortelano <ricardo.hortelano@gmail.com>
2022-04-19 23:22:35 +0200

    remove jquery and add page handling

--- js/main.js
+++ js/main.js
@@ -1,13 +1,35 @@
 console.log('js is working');
 
-$(document).ready(function () {
+window.onload = function(){
+    if (window.location.pathname === "/index.html"){
+        pageParam = new URL(location.href).searchParams.get("page")
+        console.log(pageParam)
+        if (pageParam == null){
+            window.history.pushState({}, '', window.location.origin)
+        }else{
+            window.history.pushState({}, '', pageParam)
+        }
+    }
+    load_content()
+}
 
-    $('.menu-item').click(function () {
-        dataFile = $(this).attr('data-file');
-        dataFile = `content/${dataFile}`;
-        $('section.content').html('Loading...');
-        $.get(dataFile, function (data) {
-            $('section.content').html(marked.parse(data));
-        }, "text");
-    });
+load_content = function(){
+    dataFileName = window.location.href.split("/").at(-1)
+
+    if (dataFileName !== ""){
+        data = "content/" + window.location.href.split("/").at(-1) + ".md"
+        fetch(data)
+            .then(response => response.text())
+            .then(text => document.querySelector("section.content").innerHTML = marked.parse(text))
+    }
+}
+
+// add click event to menu items
+document.querySelectorAll(".menu-item").forEach(item => {
+    item.addEventListener("click", ()=>{
+        window.history.pushState({}, '', item.getAttribute("data-file").split(".")[0])
+        window.dispatchEvent(new Event('popstate'));
+    })
 });
+
+window.addEventListener('popstate', load_content);
